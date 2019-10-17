9ce8350887dfd1a6bf436d0fe03777f58c8fc61a
github username <github email address>
2019-10-16 21:04:01 -0700

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -11,6 +11,16 @@ class Timer extends Component {
   }
 
   //Your code here
+  componentDidUpdate() {
+    this.timer.current.style.color ="#" + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  };
 
   componentDidMount() {
     this.interval = setInterval(
@@ -28,7 +38,7 @@ class Timer extends Component {
     return (
       <section className="Timer" style={{ background: color }} ref={this.timer}>
         <h1>{time}</h1>
-        <button onClick={this.stopClock}>Stop</button>
+        <button onClick={this.stopClock}>Pause</button>
         <aside className="logText">{logText}</aside>
         <small onClick={this.handleClose}>X</small>
       </section>
