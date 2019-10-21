2b585679e1e114943cfb7d879e9b2ad2e13532e4
github username <josephtrettevik@gmail.com>
2019-10-21 11:43:49 -0700

    Changed Timer to extend PureComponent instead of Component and remove the shouldComponentUpdate method

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-class Timer extends Component {
+class Timer extends PureComponent {
   constructor() {
     super();
     this.timer = React.createRef();
@@ -25,13 +25,6 @@ class Timer extends Component {
 
   componentDidUpdate() {
     this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.state.time === nextState.time) {
-      return false
-    }
-    return true
   }
 
   render() {
